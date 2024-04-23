584ba816c6da9befb6688be1b11de182196ffc31
Davsmusk <167246670+Davsmusk@users.noreply.github.com>
2024-04-23 16:05:35 -0600

    Add files via upload

---
+++ #diccionarios.py
@@ -0,0 +1,11 @@
+#diccionarios
+nombre=input("Ingrese su nombre: ")
+edad=input("Ingrese su edad: ")
+direccion=input("Ingrese su dirección: ")
+telefono= input("Ingrese su número de teléfono: ")
+
+datos={"Nombre":nombre,
+       "Edad":edad,
+       "Dirección":direccion,
+       "Teléfono":telefono }
+print(datos)
